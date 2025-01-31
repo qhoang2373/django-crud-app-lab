c6e7bcfc545d443bca1264edf26edc17c2406a32
Quan <qhoang2373@gmail.com>
2025-01-30 22:12:30 -0800

    create 2nd model

--- nationalparks/my_app/models.py
+++ nationalparks/my_app/models.py
@@ -14,3 +14,19 @@ class NationalPark(models.Model):
 
     def get_absolute_url(self):
         return reverse('park-detail', kwargs={'park_id': self.id})
+
+
+class Trail(models.Model):
+    name = models.CharField(max_length=255)
+    difficulty = models.CharField(max_length=20, choices=[
+        ('E', 'Easy'),
+        ('M', 'Moderate'),
+        ('D', 'Difficult'),
+    ])
+    description = models.CharField(Max_length=255)
+
+    national_park = models.ForeignKey(NationalPark, on_delete=models.CASCADE, related_name='trails')
+
+
+    def __str__(self):
+        return f"{self.get_difficulty_display()} on {self.name}"
